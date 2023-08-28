8194514847db40a1511c397820b262b7141ceb4f
yangdongnan <nan1010082085@163.com>
2023-08-28 09:59:03 +0800

    pref: 增加不同环境配置文件

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,10 +1,20 @@
 import { readFileSync } from 'fs';
 import * as yaml from 'js-yaml';
 import { join } from 'path';
+import { merge } from 'lodash';
 
-const YAML_CONFIG_NAME = 'config.yml';
-const defaultFilePath = join(__dirname, '../config', YAML_CONFIG_NAME);
+const DEFAULT_CONFIG = 'config.yml';
+const DEVELOPMENT_CONFIG = 'config.development.yml';
+const PRODUCTION_CONFIG = 'config.production.yml';
 
-export default () => {
-  return yaml.load(readFileSync(defaultFilePath, 'utf8'));
+const ENV = process.env.NODE_ENV;
+const CURRENT = ENV === 'development' ? DEVELOPMENT_CONFIG : PRODUCTION_CONFIG;
+
+const paramConfigByYml = (yml) => {
+  const ymlFIle = readFileSync(join(__dirname, '../config', yml), 'utf8');
+  return yaml.load(ymlFIle);
 };
+
+const C = merge(paramConfigByYml(DEFAULT_CONFIG), paramConfigByYml(CURRENT));
+
+export default () => C;
