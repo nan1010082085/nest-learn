6bcacde193b725373a039bc3fcfe4fe452bd04bb
yangdongnan <nan1010082085@163.com>
2023-09-07 15:07:23 +0800

    feat(role): 增加角色对应ID枚举

---
+++ src/enum/role.num.ts
@@ -0,0 +1,15 @@
+// 角色对应的ID
+export enum RoleEnum {
+  // 数据
+  DATA_ADMIN = 2,
+  // 普通
+  GENERAL_ADMIN = 3,
+  // 应用
+  APPLICATION_ADMIN = 4,
+  // 审计
+  lOGS_ADMIN = 5,
+  // 系统
+  SYSTEM_ADMIN = 6,
+  // 测试
+  TEST_ADMIN = 8,
+}
